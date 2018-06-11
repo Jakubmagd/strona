939ea97ee23add0f4de4277f48a0c34ac508ce48
Jakub <Jakub@JAKUB-PC>
2018-06-11 19:39:00 +0200

    Add project files.

---
+++ strona/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="strona.MvcApplication" Language="C#" %>
---
+++ strona/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(strona.Startup))]
+namespace strona
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
